3a66b49e7bd57fb8f76a3cde3acc7c48c01a8130
Marwa-Abdelrhman <32242286+Marwa-Abdelrhman@users.noreply.github.com>
2022-12-22 16:29:07 +0200

    Update main.c

--- APP/main.c
+++ APP/main.c
@@ -2,54 +2,37 @@
 
 #include <stdlib.h>
 #include <stdint.h>
-#include "GPIO.h"
-#include "SysTick.h"
+#include "/MCAL/GPIO.h"
+#include "/MCAL/SysTick.h"
 
 
 
 
-volatile uint32_t seconds;
+ //volatile uint32_t seconds;
+ uint32_t ON_Time=2 ;    
+ uint32_t OFF_Time=1;
 
-uint32_t ON_Time ;    
-uint32_t OFF_Time;
-uint16_t button_clicked_count;
 
-void (*SysTick_Cbk_ptr)(void);
-void SysTick_CallBack(void);
-void GPT_CallBack_Handler(void);
-Port_PinConfigType button,led;
 
 int main()
 {
 	
 	/*GPIO Init for buttons and led*/
-
-	button.port=GPIO_F;
-	button.direction=INPUT;
-	button.pin=PIN0;
-
-
-	led.port=GPIO_A;
-	led.direction=OUTPUT;
-	led.pin=PIN3;
-
+  GPIO_RCGEnablePorts();
+	
 	/*SysTick Timer Init*/
 	SysTick_Init();
-	SysTick_Cbk_ptr=SysTick_CallBack;
-	/*enable clk to GPIO A , GPIO F*/
-	 SYSCTL->RCGCGPIO |= 0x21;
+	//SysTick_Cbk_ptr=SysTick_CallBack;
 	
+	GPIO_SetPin(GPIO_PortA, GPIO_Pin0, GPIO_PinOut);
 	while(1)
 	{
-		
+		GPIO_WriteChannel(GPIO_PortA,GPIO_Pin0, GPIO_LevelHigh);
+		STK_SetWait(ON_Time);
+		GPIO_WriteChannel(GPIO_PortA,GPIO_Pin0, GPIO_LevelLow);
+		STK_SetWait(OFF_Time);
 		/* number of button clicked is equal seconds count so: ON_time=2 *seconds, OFF_time=seconds*/
-		if(GPIO_PORT_A_DATA==1)
-		{
-			button_clicked_count++;
-			ON_Time= 2*button_clicked_count;
-			OFF_Time=3*button_clicked_count;
-		
-		}
+
 			
 	
 	}
@@ -57,7 +40,7 @@ int main()
 	return 0;
 }
 
-void SysTick_CallBack(void)
+/*void SysTick_CallBack(void)
 {
 	
 
@@ -67,13 +50,13 @@ void SysTick_CallBack(void)
 	if(seconds == ON_Time) 
 	 
 	{
-	   Port_Write(&led,1);
+	   //Port_Write(&led,1);
 		 seconds=0;
 	}
 	
 	if(seconds == OFF_Time)  
 	{
-	  Port_Write(&led,0);
+	  //Port_Write(&led,0);
 		seconds=0;
 	}
 		
@@ -83,4 +66,4 @@ void SysTick_CallBack(void)
 	GPIO_PORT_A_DATA=0;*/
 	
 
-}
+//}*/
